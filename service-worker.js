7eea0cd5c6e7ecbd9398ff414d49ca6e69b847c6
Jujube711 <2168831960@qq.com>
2026-08-03 12:07:36 +0800

    增加飞书任务一键导入 (#8)
    Co-authored-by: Jujube711 <281832358+Jujube711@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='eisenhower-mobile-v5';
+const CACHE_NAME='eisenhower-mobile-v6';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./favicon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
