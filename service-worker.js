cb7ec4e76df8c42c146ad92ec2f1ff74e66ae59a
Jujube711 <281832358+Jujube711@users.noreply.github.com>
2026-08-03 14:18:15 +0800

    提升飞书截图识别准确率

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='eisenhower-mobile-v7';
+const CACHE_NAME='eisenhower-mobile-v8';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./favicon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
